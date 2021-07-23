1a035e984cc8371967f95acd7f5209d8d3cfd106
karan sivarat <bomb0069@gmail.com>
2021-07-23 14:18:26 +0700

    [Added]: Feature Start with Exclude

--- src/test/java/MyRangeTest.java
+++ src/test/java/MyRangeTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyRangeTest {
@@ -11,5 +12,13 @@ public class MyRangeTest {
         MyRange myRange = new MyRange("[1,5]");
         boolean result = myRange.isStartWithInclude();
         assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล (1,5] นั้นจะขึ้นต้นด้วย ( (Exclude) ผลที่ได้คือ false")
+    public void startWithExclude() {
+        MyRange myRange = new MyRange("(1,5]");
+        boolean result = myRange.isStartWithInclude();
+        assertFalse(result);
     }
 }
